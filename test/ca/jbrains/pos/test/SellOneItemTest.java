08c41ff3005aa208cd63a8aae58befbef6741945
pschwarz <philip.johann.schwarz@gmail.com>
2015-04-06 19:50:44 +0100

    removed duplicate test setup

--- test/ca/jbrains/pos/test/SellOneItemTest.java
+++ test/ca/jbrains/pos/test/SellOneItemTest.java
@@ -1,5 +1,6 @@
 package ca.jbrains.pos.test;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Collection;
@@ -11,16 +12,24 @@ import static org.junit.Assert.assertEquals;
 
 public class SellOneItemTest
 {
-    @Test
-    public void productFound() throws Exception
+
+    private Display display;
+    private Sale sale;
+
+    @Before
+    public void setUp() throws Exception
     {
-        final Display display = new Display();
-        final Sale sale = new Sale(display, new HashMap<String, String>()
+        display = new Display();
+        sale = new Sale(display, new HashMap<String, String>()
         {{
                 put("12345", "$7.95");
                 put("23456", "$12.50");
             }});
+    }
 
+    @Test
+    public void productFound() throws Exception
+    {
         sale.onBarcode("12345");
         assertEquals("$7.95", display.getText());
     }
@@ -28,13 +37,6 @@ public class SellOneItemTest
     @Test
     public void anotherProductFound() throws Exception
     {
-        final Display display = new Display();
-        final Sale sale = new Sale(display, new HashMap<String, String>()
-        {{
-                put("12345", "$7.95");
-                put("23456", "$12.50");
-            }});
-
         sale.onBarcode("23456");
         assertEquals("$12.50", display.getText());
     }
@@ -42,13 +44,6 @@ public class SellOneItemTest
     @Test
     public void productNotFound() throws Exception
     {
-        final Display display = new Display();
-        final Sale sale = new Sale(display, new HashMap<String, String>()
-        {{
-                put("12345", "$7.95");
-                put("23456", "$12.50");
-            }});
-
         sale.onBarcode("99999");
         assertEquals("Product not found for 99999", display.getText());
     }
